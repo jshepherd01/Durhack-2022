d3c7a4d06049dc4eebcb6af72928852eb5c61fb2
Joe Shepherd <sudo.jshepherd01@gmail.com>
2022-02-19 22:58:22 +0000

    Fix falling cans teleporting preemptively

--- Assets/Scripts/DamageCollision.cs
+++ Assets/Scripts/DamageCollision.cs
@@ -7,12 +7,14 @@ public class DamageCollision : MonoBehaviour
     [SerializeField] private int damage;
     [SerializeField] private bool vanish;
     public HealthController _healthController;
+    public Sprite transparent;
 
     private void OnTriggerEnter2D(Collider2D Collision) {
         if (Collision.CompareTag("Player")) {
             _healthController.DoDamage(damage);
             if (vanish) {
-                GetComponent<SpriteRenderer>().enabled = false;
+                GetComponent<SpriteRenderer>().sprite = transparent;
+                GetComponent<BoxCollider2D>().enabled = false;
             }
         }
     }
--- Assets/Scripts/TrashMove.cs
+++ Assets/Scripts/TrashMove.cs
@@ -6,15 +6,20 @@ public class TrashMove : MonoBehaviour
 {
     private Vector3 StartPosition;
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Sprite normal;
 
     void Start() {
         StartPosition = transform.position;
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        normal = sr.sprite;
     }
 
     void OnBecameInvisible() {
         transform.position = StartPosition;
-        GetComponent<SpriteRenderer>().enabled = true;
+        sr.sprite = normal;
+        GetComponent<BoxCollider2D>().enabled = true;
         rb.velocity = Vector2.zero;
         rb.angularVelocity = 0.0F;
     }
